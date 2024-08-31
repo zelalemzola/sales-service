e6ebf6d861aee6d7558906585c4184c416add213
zelalemzola <tzelalemtesfaye@gmail.com>
2024-08-31 15:38:29 +0300

    navbar done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
+
 const inter = Poppins({ subsets: ["latin"], weight:['300', '400', '500', '600', '700', '800', '900'] });
 
 export const metadata = {
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        
+        {children}
+       
+        </body>
     </html>
   );
 }
--- src/app/workers/page.jsx
+++ src/app/workers/page.jsx
@@ -195,7 +195,7 @@ const Workers = () => {
           </DialogContent>
         </Dialog>
       </div>
-
+  
       {(!maidsData || !categoriesData)?<div className='pt-[40%] text-xl text-secondary font-bold mx-auto'>Loading Workers...</div>
       :
             <div className="flex flex-wrap items-center justify-center gap-x-2 md:gap-x-8 gap-y-20 pt-[20%] md:pt-[15%] z-0 ">
