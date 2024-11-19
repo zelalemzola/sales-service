f65401a48350f870fab87338a7ad337c8cd9aca2
zelalemzola <tzelalemtesfaye@gmail.com>
2024-11-19 15:59:31 +0300

    done

--- src/app/workers/page.jsx
+++ src/app/workers/page.jsx
@@ -199,7 +199,7 @@ const Workers = () => {
   
       {(!maidsData || !categoriesData)?<div className='pt-[40%] text-xl text-secondary font-bold mx-auto'>Loading Workers...</div>
       :
-            <div className="flex flex-wrap items-center justify-center gap-x-2 md:gap-x-8 gap-y-20 pt-[22%] md:pt-[15%] z-0 pb-10">
+            <div className="flex flex-wrap items-center justify-center gap-x-2 md:gap-x-8 gap-y-20 pt-[25%] md:pt-[15%] z-0 pb-10">
         {filteredMaids.map((maid) => (
           <Card key={maid._id} className="relative shadow-lg py-4 md:p-4 scale-[90%] md:scale-100 bg-primary text-white hover:shadow-xl">
             <CardHeader>
